f4ddc41597c3cce1e51f26d00b7e16042fa460a9
chrisdesktopreplacement <chrishe44@gmail.com>
2022-10-24 22:32:24 -0500

    addinrandomorder now works

--- src/AnalysisNumber.java
+++ src/AnalysisNumber.java
@@ -10,44 +10,20 @@ public class AnalysisNumber {
         long endTime;
         List<Integer> arra = new ArrayList<Integer>();
         List<Integer> link = new LinkedList<Integer>();
-
-        Scanner in = new Scanner(System.in); 
-		System.out.println("Enter the inital list size:");
+/*         Scanner in = new Scanner(System.in); 
+		System.out.println("Enter the Initial list size:");
 		String snumb = in.nextLine(); 
-		int numb = Integer.valueOf(snumb);
+		int numb = Integer.valueOf(snumb); */
+        int numb = 100;
 
+        addInReverseOrder(arra,numb);
+        addInReverseOrder(link,numb);
 
-
-    
-/*         startTime = System.currentTimeMillis();
-        addInOrder(arra,numb);
-        endTime = System.currentTimeMillis();
-        long durationarrorder = endTime - startTime;
         arra.clear();
-        System.out.println(durationarrorder);
 
-        startTime = System.currentTimeMillis();
-        addInOrder(link,numb);
-        endTime = System.currentTimeMillis();
-        long durationlinkorder = endTime - startTime;
-        link.clear();
-        System.out.println(durationlinkorder); */
 
-        startTime = System.currentTimeMillis();
-        addInRandomOrder(arra,numb);
-        endTime = System.currentTimeMillis();
-        long durationarrrev = endTime - startTime;
-        arra.clear();
-        System.out.println(durationarrrev);
 
-        startTime = System.currentTimeMillis();
-        addInRandomOrder(link,numb);
-        endTime = System.currentTimeMillis();
-        long durationlinkrev = endTime - startTime;
-        link.clear();
-        System.out.println(durationlinkrev);
-
-        in.close();
+/*        in.close(); */
     }
 
     public static int binarySearch(List<Integer> numbers, int number){
@@ -75,25 +51,29 @@ public class AnalysisNumber {
     }
     
     public static void addInReverseOrder(List<Integer> numbers, int n){
-        for(int i =n+1; i>0;i--){
 
-            int index = binarySearch(numbers, i);
-            numbers.add(index,index);
+        for(int i = n+1;i>0;i--){
+            int index = Collections.binarySearch(numbers,i);
+            int indextest = indexFixer(index);
+            numbers.add(indextest,indextest);
         }
-        System.out.println(numbers);
     }
-    
     public static void addInRandomOrder(List<Integer> numbers, int n){
-        List<Integer> random=new ArrayList<Integer>();
-        for(int i = 0;i<n+1;i++){
+        ArrayList<Integer> random = new ArrayList<Integer>();
+        for(int i =0; i<n+1;i++){
             random.add(i);
         }
         Collections.shuffle(random);
-
-        for(int i =0; i<n+1;i++){
-            int index =binarySearch(random,i);
-            numbers.add(index,index);
+        for(int i=0;i<n+1;i++){
+            numbers.add(null);
         }
+
+        for(int i=0;i<n+1;i++){
+            int test = random.get(i);
+            numbers.set(test,test);
+        }
+
     }
 
+    
 }
